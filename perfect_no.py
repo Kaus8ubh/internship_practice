25087d119d0c3881cf8263ef54170eea8dda7661
Kaus8ubh <kaustubhbhusane81198@gmail.com>
2025-01-07 09:54:23 +0000

    perfect numbers

---
+++ perfect_no.py
@@ -0,0 +1,9 @@
+##perfect numbers from 1 to 100
+for i in range(2,101):
+  sum=0
+  for j in range(1,i):
+    if i%j==0:
+      sum+=j
+  if sum==i:
+    print(i)
+git clone https://github.com/<your-username>/<your-repository>.git
