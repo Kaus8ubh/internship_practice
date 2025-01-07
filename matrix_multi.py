d0b91cdc183cf031f77720322bd13255030d9eed
Kaus8ubh <kaustubhbhusane81198@gmail.com>
2025-01-07 17:25:09 +0000

    minimized code for multiplication

--- matrix_multi.py
+++ matrix_multi.py
@@ -3,33 +3,22 @@
 R1 = int(input("Enter the number of rows"))
 C1 = int(input("Enter the number of columns"))
 arr1 = []
-
 for i in range(R1):          
     a =[]
     for j in range(C1):      
          a.append(int(input()))
     arr1.append(a)
 
-for i in range(R1):
-    for j in range(C1):
-        print(arr1[i][j],end=' ')
-    print()
-
 R2 = int(input("Enter the number of rows"))
 C2 = int(input("Enter the number of columns"))
 arr2 = []
-
 for i in range(R2):          
     a =[]
     for j in range(C2):      
          a.append(int(input()))
     arr2.append(a)
 
-for i in range(R2):
-    for j in range(C2):
-        print(arr2[i][j],end=' ')
-    print()
-if C1==R2:
+if C1==R2:     ##condition for matrix multiplication
   result = [[0 for j in range(C2)] for i in range(R1)]
   for i in range(len(arr1)):
     for j in range(len(arr2[0])):
