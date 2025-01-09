d919eaae09ffd341e565ef909332f3943074740e
Kaus8ubh <kaustubhbhusane81198@gmail.com>
2025-01-09 23:08:02 +0530

    Update frequency_of_words.py

--- frequency_of_words.py
+++ frequency_of_words.py
@@ -1,5 +1,8 @@
-para=["A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic. Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs. This is because paragraphs show a reader where the subdivisions of an essay begin and end, and thus help the reader see the organization of the essay and grasp its main points"]
-
+para = """ A paragraph is a series of sentences that are organized and coherent, and are all related 
+           to a single topic. Almost every piece of writing you do that is longer than a few sentences 
+           should be organized into paragraphs. This is because paragraphs show a reader where the subdivisions 
+           of an essay begin and end, and thus help the reader see the organization of the essay and grasp its main points """
+      
 def occurance_of_words(para):
   dict={}
   drops=["A","a","the","The","is","Is","of","Of","and",
